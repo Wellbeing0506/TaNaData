30c360e83259c536d8eae6a2a3019b807417ef3b
dave <well1001@hotmail.com>
2017-09-26 17:22:20 +0800

    start again

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var logger = require('tracer').colorConsole();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	logger.log("demo");	
-  res.render('index', { title: '智工網' });
+  res.render('index', { title: 'demo' });
 });
 
 module.exports = router;
