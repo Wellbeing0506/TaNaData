daa80ba0ba04073c70fb96c65ac5df58800172f6
dave <well1001@hotmail.com>
2017-09-26 22:30:59 +0800

    test chinese

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'demo' });
+  res.render('index', { title: '智工網' });
 });
 
 module.exports = router;
