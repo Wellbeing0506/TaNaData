1f3bf86da62815a02039d207af79e645a8f44a1c
dave <well1001@hotmail.com>
2017-09-26 15:28:25 +0800

    add logger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var logger = require('tracer').colorConsole();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '智供網' });
+	logger.log("demo");	
+  res.render('index', { title: '智工網' });
 });
 
 module.exports = router;
