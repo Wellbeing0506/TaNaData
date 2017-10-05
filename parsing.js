977d0ea7883a7ff3bc90e1160caf52ca766ea7d1
dave <well1001@hotmail.com>
2017-10-05 22:39:40 +0800

    test paring url in paring.js

--- parsing.js
+++ parsing.js
@@ -1,6 +1,6 @@
-var request = require('request');
-request(
-    {url : "http://v.youku.com/v_show/id_XMzA2NTg1MzI2NA==.html?spm=a2hww.20027244.m_250166.5~5!2~5~5!2~5~1!2~3~A"},
-    function(error,res,body){
-         console.log(body); 
-});
+var cheerio = require('cheerio');
+var url = "http://v.youku.com/v_show/id_XMzA2NTg1MzI2NA==.html?spm=a2hww.20027244.m_250166.5~5!2~5~5!2~5~1!2~3~A";
+var re = /id_(\S{13})NA/;
+
+var aaa = re.exec(url);
+console.log(aaa);
