a0707510cf6829b329fed3ccfaf561dc34e288a1
dave <well1001@hotmail.com>
2017-10-06 10:40:32 +0800

    add nameList

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var nameList = require('../config/nameList.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index.ejs',{});
+	console.log(nameList);
+	res.render('index.ejs',{nameList:nameList});
 });
 
 
